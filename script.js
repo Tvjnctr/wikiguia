fef3c7d883f85ccb937d1f71bf973b7db956787d
VvN123 <heero7_@hotmail.com>
2023-05-09 01:07:14 -0400

    Archivo javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+let loginForm = document.getElementById("formulario");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let usuario = document.getElementById("input_usuario");
+  let pass = document.getElementById("input_pass");
+
+  if (usuario.value == "" || pass.value == "") {
+    alert("Existen campos vacios!");
+  } 
+  else if ((usuario.value).length > 20 || (usuario.value).length < 3 ){
+    alert("El nombre es incorrecto!");
+  }
+  else {
+    // perform operation with form input
+    alert("Login Correcto");
+    console.log(
+      `usuario ${usuario.value}
+       pass ${pass.value}`
+    );
+
+    usuario.value = "";
+    pass.value = "";
+  }
+});
