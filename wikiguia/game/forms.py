403099238da9b9b26d72f1b28180adffaf2b0bdf
VvN123 <heero7_@hotmail.com>
2023-06-19 07:50:01 -0400

    se crea formulario para juego nuevo.

---
+++ wikiguia/game/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Game
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Game
+        fields = ('name','description','release_date','is_visible','image','carousel_image',)
+        widgets = {
+            'name' : forms.TextInput(attrs={
+                'class' : 'input_form_box'
+            }),
+            'description' : forms.Textarea(attrs={
+                'class' : 'input_form_box'
+            }),
+            'release_date' : forms.DateInput(attrs={
+                'class' : 'input_form_box'
+            }),
+            'is_visible' : forms.CheckboxInput(attrs={
+                'class' : 'input_form_box'
+            }),            
+            'image' : forms.FileInput(attrs={
+                'class' : 'input_form_box'
+            }),
+            'carousel_image' : forms.FileInput(attrs={
+                'class' : 'input_form_box'
+            }),            
+        }
--- wikiguia/game/views.py
+++ wikiguia/game/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Game
+from .forms import NewItemForm
 
 def detail(request, pk):
     game = get_object_or_404(Game, pk=pk)
 
     return render(request, 'game/detail.html', {
         'game' : game
+    })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            game = form.save(commit=False)
+            game.save()
+
+            return redirect('game:detail', pk=game.id)
+
+    else:
+        form = NewItemForm()
+
+    return render(request, 'game/form.html', {
+        'form':form
     })
