f9fc376e71709966b416ebab24b7b505fc67a4db
VvN123 <heero7_@hotmail.com>
2023-06-22 10:36:47 -0400

    Se cambian labels en form the GAME

--- wikiguia/game/forms.py
+++ wikiguia/game/forms.py
@@ -5,6 +5,15 @@ class NewItemForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ('name','description','description_short','release_date','is_visible','image','carousel_image',)
+        labels = {
+            'name': 'Nombre',
+            'description': 'Descripción',
+            'description_short': 'Descripción breve',
+            'release_date': 'Fecha de Lanzamiento',
+            'is_visible': 'Publicar al crear',
+            'image': 'Imagen',
+            'carousel_image': 'Imagen para Carousel (opcional)',
+        }
         widgets = {
             'name' : forms.TextInput(attrs={
                 'class' : 'input_form_box'
