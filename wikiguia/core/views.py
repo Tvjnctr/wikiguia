0ab35d68fa5b59bdc938ef4123b0b5c8ff98c1e9
VvN123 <heero7_@hotmail.com>
2023-06-22 18:26:06 -0400

    cambios en estilo guias

--- wikiguia/core/views.py
+++ wikiguia/core/views.py
@@ -4,7 +4,7 @@ from . forms import SignupForm
 # Create your views here.
 
 def index(request):  
-    games = Game.objects.filter(is_visible=True)[:3]  
+    games = Game.objects.filter(is_visible=True)[:6]  
     return render(request, 'core/index.html',{
         'games' : games,
     })
