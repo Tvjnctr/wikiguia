962db817b5dfb87c8c09fa8c5e537d0c7447e181
VvN123 <heero7_@hotmail.com>
2023-06-23 10:46:35 -0400

    se modifica login

--- wikiguia/core/forms.py
+++ wikiguia/core/forms.py
@@ -4,13 +4,15 @@ from django.contrib.auth.models import User
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder':'Tu Usuario',
-        'class':'input_form_box',
+        'placeholder':'Ingresa nombre de Usuario',
+        'class':'input_form_box login-container-form',
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder':'Ingresa Contraseña',
-        'class':'input_form_box',
+        'class':'input_form_box login-container-form',
     }))
+
+
 
 class SignupForm(UserCreationForm):
     class Meta:
